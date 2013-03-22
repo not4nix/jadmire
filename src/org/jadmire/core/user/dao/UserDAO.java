00c7d78b18a939608a0424b659cb8bfd97825dbe
Viacheslav Okolitiy <justforgit@gmail.com>
2013-03-22 08:27:50 +0200

    Minor changes to user dao

--- src/org/jadmire/core/user/dao/UserDAO.java
+++ src/org/jadmire/core/user/dao/UserDAO.java
@@ -1,4 +1,25 @@
 package org.jadmire.core.user.dao;
 
-public interface UserDAO {
+import org.hibernate.HibernateException;
+import org.jadmire.core.dao.Dao;
+import org.jadmire.core.user.domain.User;
+
+import java.util.List;
+
+public interface UserDAO extends Dao<User> {
+
+    @Override
+    void delete(Integer entityId) throws HibernateException;
+
+    @Override
+    User read(Integer id) throws HibernateException;
+
+    @Override
+    List<User> readAll() throws HibernateException;
+
+    @Override
+    Integer save(String name) throws HibernateException;
+
+    @Override
+    void update(Integer entityId, String entityName) throws HibernateException;
 }
