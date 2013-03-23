c1583e998b58e6639b45f1be7ed1a0d3f74737a6
Viacheslav Okolitiy <justforgit@gmail.com>
2013-03-23 02:11:42 +0200

    Added hibernate util

--- src/org/jadmire/core/user/dao/impl/UserDAOImpl.java
+++ src/org/jadmire/core/user/dao/impl/UserDAOImpl.java
@@ -1,6 +1,34 @@
 package org.jadmire.core.user.dao.impl;
 
+import org.hibernate.HibernateException;
 import org.jadmire.core.user.dao.UserDAO;
+import org.jadmire.core.user.domain.User;
+
+import java.util.List;
 
 public class UserDAOImpl implements UserDAO {
+    @Override
+    public void delete(Integer entityId) throws HibernateException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public User read(Integer id) throws HibernateException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public List<User> readAll() throws HibernateException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Integer save(String name) throws HibernateException {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void update(Integer entityId, String entityName) throws HibernateException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
---
+++ src/org/jadmire/util/HibernateUtil.java
@@ -0,0 +1,26 @@
+package org.jadmire.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static SessionFactory mSessionFactory;
+
+    static {
+        try {
+            buildSessionFactory();
+        } catch (Throwable throwable){
+            System.err.println("Initial SessionFactory creation failed." + throwable);
+            throw new ExceptionInInitializerError(throwable);
+        }
+    }
+
+    public static void buildSessionFactory(){
+        mSessionFactory = new Configuration().configure().buildSessionFactory();
+    }
+
+
+    public static SessionFactory getSessionFactory() {
+        return mSessionFactory;
+    }
+}
